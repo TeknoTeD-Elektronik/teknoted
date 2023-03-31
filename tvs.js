cee9141471f4906cc72daa0d33bb8b3be91377a4
TeknoTeD-Elektronik <129218050+TeknoTeD-Elektronik@users.noreply.github.com>
2023-03-31 12:00:33 +0100

    Add files via upload

---
+++ tvs.js
@@ -0,0 +1,15 @@
+var tv1 = document.getElementById("tv1");
+var tv2 = document.getElementById("tv2");
+var tv3 = document.getElementById("tv3");
+
+tv1.addEventListener("click", function() {
+  // Show TV 1 info
+});
+
+tv2.addEventListener("click", function() {
+  // Show TV 2 info
+});
+
+tv3.addEventListener("click", function() {
+  // Show TV 3 info
+});
